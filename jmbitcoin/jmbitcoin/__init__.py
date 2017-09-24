07dbbc26e36838df91867c8a16b0a2b40f9be897
AdamISZ <ekaggata@gmail.com>
2017-09-24 22:09:12 +0200

    add opcodes to jmbitcoin for downstream custom scripts

--- jmbitcoin/jmbitcoin/__init__.py
+++ jmbitcoin/jmbitcoin/__init__.py
@@ -3,4 +3,5 @@ from jmbitcoin.secp256k1_main import *
 from jmbitcoin.secp256k1_transaction import *
 from jmbitcoin.secp256k1_deterministic import *
 from jmbitcoin.bci import *
+from jmbitcoin.btscript import *
 
