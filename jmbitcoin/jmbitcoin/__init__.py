83a126306a0506bd78948f172876a07573bda992
AdamISZ <ekaggata@gmail.com>
2018-12-19 20:34:03 +0100

    remove unused bci.py module

--- jmbitcoin/jmbitcoin/__init__.py
+++ jmbitcoin/jmbitcoin/__init__.py
@@ -5,7 +5,6 @@ import coincurve as secp256k1
 from jmbitcoin.secp256k1_main import *
 from jmbitcoin.secp256k1_transaction import *
 from jmbitcoin.secp256k1_deterministic import *
-from jmbitcoin.bci import *
 from jmbitcoin.btscript import *
 from jmbitcoin.bech32 import *
 
