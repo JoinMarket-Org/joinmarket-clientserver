JoinMarket-Org/joinmarket-clientserver

commit c139067be48377dba9d29fc45c1424d199676a9a

Include chromalog package for colorized logs Also use this lib to print non-log messages with standardized color formats. Some clean up in jmbase, remove unused objects/methods.

jmbase/jmbase/support.py
@@ -1,16 +1,48 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import * # noqa: F401
-from future.utils import iteritems
-
-import sys
 
 import logging
-import pprint
 from getpass import getpass
 
-logFormatter = logging.Formatter(
-    "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+from chromalog.log import (
+    ColorizingStreamHandler,
+    ColorizingFormatter,
+)
+from chromalog.colorizer import GenericColorizer
+from colorama import Fore, Back, Style
+
+# magic; importing e.g. 'info' actually instantiates
+# that as a function that uses the color map
+# defined below. ( noqa because flake doesn't understand)
+from chromalog.mark.helpers.simple import (  # noqa: F401
+    debug,
+    info,
+    important,
+    success,
+    warning,
+    error,
+    critical,
+)
+
+# our chosen colorings for log messages in JM:
+jm_color_map = {
+    'debug': (Style.DIM + Fore.LIGHTBLUE_EX, Style.RESET_ALL),
+    'info': (Style.BRIGHT + Fore.BLUE, Style.RESET_ALL),
+    'important': (Style.BRIGHT, Style.RESET_ALL),
+    'success': (Fore.GREEN, Style.RESET_ALL),
+    'warning': (Fore.YELLOW, Style.RESET_ALL),
+    'error': (Fore.RED, Style.RESET_ALL),
+    'critical': (Back.RED, Style.RESET_ALL),
+}
+
+class JMColorizer(GenericColorizer):
+    default_color_map = jm_color_map
+
+jm_colorizer = JMColorizer()
+
+logFormatter = ColorizingFormatter(
+    "%(asctime)s [%(levelname)s]  %(message)s")
 log = logging.getLogger('joinmarket')
 log.setLevel(logging.DEBUG)
 
@@ -21,11 +53,10 @@ debug_silence = [False]
 #TODO pass this through from client, bitcoin paramater:
 DUST_THRESHOLD = 2730
 
-#consoleHandler = logging.StreamHandler(stream=sys.stdout)
-class JoinMarketStreamHandler(logging.StreamHandler):
+class JoinMarketStreamHandler(ColorizingStreamHandler):
 
-    def __init__(self, stream):
-        super(JoinMarketStreamHandler, self).__init__(stream)
+    def __init__(self):
+        super(JoinMarketStreamHandler, self).__init__(colorizer=jm_colorizer)
 
     def emit(self, record):
         if joinmarket_alert[0]:
@@ -35,10 +66,31 @@ class JoinMarketStreamHandler(logging.StreamHandler):
         if not debug_silence[0]:
             super(JoinMarketStreamHandler, self).emit(record)
 
+handler = JoinMarketStreamHandler()
+handler.setFormatter(logFormatter)
+log.addHandler(handler)
 
-consoleHandler = JoinMarketStreamHandler(stream=sys.stdout)
-consoleHandler.setFormatter(logFormatter)
-log.addHandler(consoleHandler)
+def jmprint(msg, level="info"):
+    """ Provides the ability to print messages
+    with consistent formatting, outside the logging system
+    (in case you don't want the standard log format).
+    Example applications are: REPL style stuff, and/or
+    some very important / user workflow affecting communication.
+    Note that this exclusively for console printout, NOT for
+    logging to file (chromalog will handle file streams
+    properly, but this will not).
+    """
+    if not level in jm_color_map.keys():
+        raise Exception("Unsupported formatting")
+
+    # .colorize_message function does a .format() on the string,
+    # which does not work with string-ified json; this should
+    # result in output as intended:
+    msg = msg.replace('{', '{{')
+    msg = msg.replace('}', '}}')
+
+    fmtfn = eval(level)
+    print(jm_colorizer.colorize_message(fmtfn(msg)))
 
 def get_log():
     """
@@ -48,7 +100,7 @@ def get_log():
     return log
 
 def set_logging_level(level):
-    consoleHandler.setLevel(level)
+    handler.setLevel(level)
 
 def chunks(d, n):
     return [d[x:x + n] for x in range(0, len(d), n)]
@@ -58,21 +110,3 @@ def get_password(msg): #pragma: no cover
     if not isinstance(password, bytes):
         password = password.encode('utf-8')
     return password
-
-def debug_dump_object(obj, skip_fields=None):
-    if skip_fields is None:
-        skip_fields = []
-    log.debug('Class debug dump, name:' + obj.__class__.__name__)
-    for k, v in iteritems(obj.__dict__):
-        if k in skip_fields:
-            continue
-        if k == 'password' or k == 'given_password':
-            continue
-        log.debug('key=' + k)
-        if isinstance(v, str):
-            log.debug('string: len:' + str(len(v)))
-            log.debug(v)
-        elif isinstance(v, dict) or isinstance(v, list):
-            log.debug(pprint.pformat(v))
-        else:
-            log.debug(str(v))

jmbase/jmbase/__init__.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
 
-from .support import (get_log, chunks, debug_silence, debug_dump_object,
+from .support import (get_log, chunks, debug_silence, jmprint,
                       joinmarket_alert, core_alert, get_password,
                       set_logging_level)
 from .commands import *

jmbase/setup.py
@@ -9,5 +9,6 @@ setup(name='joinmarketbase',
       author_email='',
       license='GPL',
       packages=['jmbase'],
-      install_requires=['future', 'twisted==18.9.0', 'service-identity'],
+      install_requires=['future', 'twisted==18.9.0', 'service-identity',
+                        'chromalog==1.0.5'],
       zip_safe=False)

jmbase/test/test_base_support.py
@@ -2,25 +2,10 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import * # noqa: F401
-from jmbase.support import debug_dump_object, joinmarket_alert
 
-def test_debug_dump_object():
-    joinmarket_alert[0] = "dummy jm alert"
-    class TestObj(object):
-        def __init__(self):
-            self.x = "foo"
-            self.password = "bar"
-            self.y = "baz"
-    to = TestObj()
-    debug_dump_object(to)
-    to.given_password = "baa"
-    debug_dump_object(to)
-    to.extradict = {1:2, 3:4}
-    debug_dump_object(to)
-    to.extralist = ["dummy", "list"]
-    debug_dump_object(to)
-    to.extradata = 100
-    debug_dump_object(to, skip_fields="y")
+def test_color_coded_logging():
+    # TODO
+    pass
 
 
 

jmclient/jmclient/blockchaininterface.py
@@ -16,7 +16,7 @@ import jmbitcoin as btc
 
 from jmclient.jsonrpc import JsonRpcConnectionError, JsonRpcError
 from jmclient.configure import get_p2pk_vbyte, jm_single
-from jmbase.support import get_log
+from jmbase.support import get_log, jmprint
 
 log = get_log()
 
@@ -388,7 +388,7 @@ class BitcoinCoreInterface(BlockchainInterface):
             if restart_cb:
                 restart_cb(restart_msg)
             else:
-                print(restart_msg)
+                jmprint(restart_msg, "important")
                 sys.exit(0)
 
     def sync_wallet(self, wallet, fast=False, restart_cb=None):
@@ -936,7 +936,7 @@ class RegtestBitcoinCoreInterface(BitcoinCoreInterface): #pragma: no cover
 
         ret = super(RegtestBitcoinCoreInterface, self).pushtx(txhex)
         if not self.simulating and self.tick_forward_chain_interval > 0:
-            print('will call tfc after ' + str(self.tick_forward_chain_interval) + ' seconds.')
+            log.debug('will call tfc after ' + str(self.tick_forward_chain_interval) + ' seconds.')
             reactor.callLater(self.tick_forward_chain_interval,
                               self.tick_forward_chain, 1)
         return ret

jmclient/jmclient/client_protocol.py
@@ -57,7 +57,7 @@ class JMClientProtocol(amp.AMP):
         d.addErrback(self.defaultErrback)
 
     def connectionMade(self):
-        print('connection was made, starting client')
+        jlog.debug('connection was made, starting client.')
         self.factory.setClient(self)
         self.clientStart()
 

jmclient/jmclient/commitment_utils.py
@@ -4,6 +4,7 @@ from builtins import * # noqa: F401
 
 import sys
 import jmbitcoin as btc
+from jmbase import jmprint
 from jmclient import jm_single, get_p2pk_vbyte, get_p2sh_vbyte
 
 def quit(parser, errmsg): #pragma: no cover
@@ -25,12 +26,12 @@ def get_utxo_info(upriv):
         assert n in range(256)
     except:
         #not sending data to stdout in case privkey info
-        print("Failed to parse utxo information for utxo")
+        jmprint("Failed to parse utxo information for utxo", "error")
         raise
     try:
         hexpriv = btc.from_wif_privkey(priv, vbyte=get_p2pk_vbyte())
     except:
-        print("failed to parse privkey, make sure it's WIF compressed format.")
+        jmprint("failed to parse privkey, make sure it's WIF compressed format.", "error")
         raise
     return u, priv
     
@@ -43,27 +44,26 @@ def validate_utxo_data(utxo_datas, retrieve=False, segwit=False):
     """
     results = []
     for u, priv in utxo_datas:
-        print('validating this utxo: ' + str(u))
+        jmprint('validating this utxo: ' + str(u), "info")
         hexpriv = btc.from_wif_privkey(priv, vbyte=get_p2pk_vbyte())
         if segwit:
             addr = btc.pubkey_to_p2sh_p2wpkh_address(
                 btc.privkey_to_pubkey(hexpriv), get_p2sh_vbyte())
         else:
             addr = btc.privkey_to_address(hexpriv, magicbyte=get_p2pk_vbyte())
-        print('claimed address: ' + addr)
+        jmprint('claimed address: ' + addr, "info")
         res = jm_single().bc_interface.query_utxo_set([u])
-        print('blockchain shows this data: ' + str(res))
         if len(res) != 1 or None in res:
-            print("utxo not found on blockchain: " + str(u))
+            jmprint("utxo not found on blockchain: " + str(u), "error")
             return False
         if res[0]['address'] != addr:
-            print("privkey corresponds to the wrong address for utxo: " + str(u))
-            print("blockchain returned address: {}".format(res[0]['address']))
-            print("your privkey gave this address: " + addr)
+            jmprint("privkey corresponds to the wrong address for utxo: " + str(u), "error")
+            jmprint("blockchain returned address: {}".format(res[0]['address']), "error")
+            jmprint("your privkey gave this address: " + addr, "error")
             return False
         if retrieve:
             results.append((u, res[0]['value']))
-    print('all utxos validated OK')
+    jmprint('all utxos validated OK', "success")
     if retrieve:
         return results
     return True

jmclient/jmclient/configure.py
@@ -12,7 +12,7 @@ from configparser import ConfigParser, NoOptionError
 import jmbitcoin as btc
 from jmclient.jsonrpc import JsonRpc
 from jmbase.support import (get_log, joinmarket_alert, core_alert, debug_silence,
-                            set_logging_level)
+                            set_logging_level, jmprint)
 from jmclient.podle import set_commitment_file
 
 log = get_log()
@@ -407,8 +407,8 @@ def load_program_config(config_path=None, bs=None):
     if len(loadedFiles) != 1:
         with open(global_singleton.config_location, "w") as configfile:
             configfile.write(defaultconfig)
-        print("Created a new `joinmarket.cfg`. Please review and adopt the "
-              "settings and restart joinmarket.")
+        jmprint("Created a new `joinmarket.cfg`. Please review and adopt the "
+              "settings and restart joinmarket.", "info")
         exit(1)
 
     #These are left as sanity checks but currently impossible
@@ -437,7 +437,8 @@ def load_program_config(config_path=None, bs=None):
     try:
         set_logging_level(loglevel)
     except:
-        print("Failed to set logging level, must be DEBUG, INFO, WARNING, ERROR")
+        jmprint("Failed to set logging level, must be DEBUG, INFO, WARNING, ERROR",
+                "error")
     try:
         global_singleton.maker_timeout_sec = global_singleton.config.getint(
             'TIMEOUT', 'maker_timeout_sec')

commit 195189a7d46840136041c057e608b0de2bf0388f

Fix "AttributeError: 'NoneType' object has no attribute 'replace'" when jmprint() called with None as msg
Bug introduced with c139067be48377dba9d29fc45c1424d199676a9a

jmbase/jmbase/support.py
@@ -83,11 +83,12 @@ def jmprint(msg, level="info"):
     if not level in jm_color_map.keys():
         raise Exception("Unsupported formatting")
 
-    # .colorize_message function does a .format() on the string,
-    # which does not work with string-ified json; this should
-    # result in output as intended:
-    msg = msg.replace('{', '{{')
-    msg = msg.replace('}', '}}')
+    if msg is not None:
+        # .colorize_message function does a .format() on the string,
+        # which does not work with string-ified json; this should
+        # result in output as intended:
+        msg = msg.replace('{', '{{')
+        msg = msg.replace('}', '}}')
 
     fmtfn = eval(level)
     print(jm_colorizer.colorize_message(fmtfn(msg)))