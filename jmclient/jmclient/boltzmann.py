5868227e350e2a2754a179f8ca865d9fd9e86b99
Martino Salvetti <the9ull@gmail.com>
2019-01-20 16:22:56 +0100

    Add boltzmann algorithm storage

---
+++ jmclient/jmclient/boltzmann.py
@@ -0,0 +1,71 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import * # noqa: F401
+
+import numbers
+
+
+def _int_to_bytestr(i):
+    return str(i).encode('ascii')
+
+
+class Boltzmann(object):
+    STORAGE_KEY = b'boltzmann'
+
+    def __init__(self, storage):
+        self.storage = storage
+        # {hex_script: int_rate}
+        self._rates = None
+        self._load_storage()
+        assert self._rates is not None
+
+    @classmethod
+    def initialize(cls, storage):
+        storage.data[cls.STORAGE_KEY] = {}
+
+    def _load_storage(self):
+        storage = self.storage.data[self.STORAGE_KEY]
+        assert isinstance(storage, dict)
+        assert all([isinstance(x, bytes) for x in storage.keys()])
+        assert all([isinstance(x, bytes) for x in storage.values()])
+
+        self._rates = {}
+        for script, rate in storage.items():
+            self._rates[script] = int(rate)
+
+    def save(self, write=True):
+        new_data = {}
+        self.storage.data[self.STORAGE_KEY] = new_data
+        for script, rate in self._rates.items():
+            rate = _int_to_bytestr(rate)
+            # storage keys must be bytes()
+            new_data[script] = rate
+        if write:
+            self.storage.save()
+
+    def reset(self):
+        self._rates = {}
+
+    def get_rate(self, script):
+        assert isinstance(script, bytes)
+
+        return self._rates.get(script, 1)
+
+    def has_script(self, script):
+        assert isinstance(script, bytes)
+
+        return script in self._rates
+
+    def remove_script(self, script):
+        assert isinstance(script, bytes)
+
+        return self._rates.pop(script)
+
+    def set_rate(self, script, rate):
+        assert isinstance(script, bytes)
+        assert isinstance(rate, numbers.Integral)
+
+        self._rates[script] = rate
+
+    def boltzmann(self, tx):
+        raise NotImplementedError
---
+++ jmclient/test/test_boltzmann.py
@@ -0,0 +1,73 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from jmclient.boltzmann import Boltzmann
+
+from test_storage import MockStorage
+import pytest
+
+from jmclient import load_program_config
+import jmclient
+from commontest import DummyBlockchainInterface
+
+
+def test_boltzmann_persist():
+    storage = MockStorage(None, 'wallet.jmdat', None, create=True)
+    Boltzmann.initialize(storage)
+    bz = Boltzmann(storage)
+
+    script0 = b'\x00' * 7
+    script1 = b'\x01' * 7
+    script2 = b'\x02' * 7
+    rate0 = 3
+    rate1 = 5 ** 13
+
+    bz.set_rate(script0, rate0)
+    bz.set_rate(script1, rate1)
+
+    bz.save()
+    del bz
+
+    bz = Boltzmann(storage)
+
+    assert bz.get_rate(script0) == rate0
+    assert bz.has_script(script0)
+    assert bz.get_rate(script1) == rate1
+    assert bz.has_script(script1)
+    assert bz.get_rate(script2) == 1
+    assert not bz.has_script(script2)
+
+    bz.remove_script(script1)
+
+    bz.save()
+    del bz
+
+    bz = Boltzmann(storage)
+
+    assert bz.has_script(script0)
+    assert not bz.has_script(script1)
+    assert not bz.has_script(script2)
+
+    bz.reset()
+    bz.save()
+    del bz
+
+    bz = Boltzmann(storage)
+
+    assert not bz.has_script(script0)
+
+
+def _test_boltzmann(setup_env_nodeps):
+    storage = MockStorage(None, 'wallet.jmdat', None, create=True)
+    Boltzmann.initialize(storage)
+    bz = Boltzmann(storage)
+
+    tx = {}  # TODO
+    bz.boltzmann(tx)
+
+
+@pytest.fixture
+def setup_env_nodeps(monkeypatch):
+    monkeypatch.setattr(jmclient.configure, 'get_blockchain_interface_instance',
+                        lambda x: DummyBlockchainInterface())
+    load_program_config()
