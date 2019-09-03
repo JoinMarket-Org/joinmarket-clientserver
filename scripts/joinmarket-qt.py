JoinMarket-Org/joinmarket-clientserver

commit 22467f436a5f5d8a4a0f3bed52c97caf2b38e3f9

Use restartForScan callback in selectWallet

scripts/joinmarket-qt.py
@@ -1451,7 +1451,7 @@ class JMMainWindow(QMainWindow):
                                    title="Wallet created")
         self.initWallet(seed=self.walletname, restart_cb=self.restartForScan)
 
-    def selectWallet(self, testnet_seed=None, restart_cb=None):
+    def selectWallet(self, testnet_seed=None):
         if jm_single().config.get("BLOCKCHAIN", "blockchain_source") != "regtest":
             current_path = os.path.dirname(os.path.realpath(__file__))
             if os.path.isdir(os.path.join(current_path, 'wallets')):
@@ -1473,7 +1473,7 @@ class JMMainWindow(QMainWindow):
                 if not ok:
                     return
                 pwd = str(text).strip()
-                decrypted = self.loadWalletFromBlockchain(firstarg[0], pwd, restart_cb)
+                decrypted = self.loadWalletFromBlockchain(firstarg[0], pwd, restart_cb=self.restartForScan)
         else:
             if not testnet_seed:
                 testnet_seed, ok = QInputDialog.getText(self,

commit f7487fee42e1cb26997d039dea4809163880e695

Fix restartForScan callback in selectWallet for regtest
Fix bug introduced with 22467f436a5f5d8a4a0f3bed52c97caf2b38e3f9

scripts/joinmarket-qt.py
@@ -1513,7 +1513,7 @@ class JMMainWindow(QMainWindow):
             firstarg = str(testnet_seed)
             pwd = None
             #ignore return value as there is no decryption failure possible
-            self.loadWalletFromBlockchain(firstarg, pwd, restart_cb)
+            self.loadWalletFromBlockchain(firstarg, pwd, restart_cb=self.restartWithMsg)
 
     def loadWalletFromBlockchain(self, firstarg=None, pwd=None, restart_cb=None):
         if firstarg: