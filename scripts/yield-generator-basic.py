72149ff63a5b12a09d955ed93d590d8d23e95274
Daniel Kraft <d@domob.eu>
2019-09-27 17:33:08 +0200

    Make yg algorithms easier to define.
    This refactors YieldGeneratorBasic a bit.  In particular, we move things
    that custom algorithms will most likely want to change to separate
    functions.  That way, it is easy to define the behaviour of inputs
    and outputs for the join without the need to duplicate the other, more
    general logic of create_my_orders and oid_to_order.

--- scripts/yield-generator-basic.py
+++ scripts/yield-generator-basic.py
@@ -3,7 +3,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import * # noqa: F401
 
-from jmbase import get_log, jmprint
+from jmbase import jmprint
 from jmclient import YieldGeneratorBasic, ygmain
 
 """THESE SETTINGS CAN SIMPLY BE EDITED BY HAND IN THIS FILE:
@@ -15,8 +15,6 @@ ordertype = 'swreloffer' #'swreloffer' or 'swabsoffer'
 nickserv_password = ''
 max_minsize = 100000
 gaplimit = 6
-
-jlog = get_log()
 
 if __name__ == "__main__":
     ygmain(YieldGeneratorBasic, txfee=txfee, cjfee_a=cjfee_a,
