b289e39085818cc3d80b077844d0ef0da720d336
Adam Gibson <ekaggata@gmail.com>
2020-07-04 12:48:52 +0100

    Update version to 0.7.0dev

--- jmdaemon/setup.py
+++ jmdaemon/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(name='joinmarketdaemon',
-      version='0.6.3.1',
+      version='0.7.0dev',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='http://github.com/Joinmarket-Org/joinmarket-clientserver/jmdaemon',
       author='',
       author_email='',
       license='GPL',
       packages=['jmdaemon'],
-      install_requires=['future', 'txtorcon', 'pyopenssl', 'libnacl', 'joinmarketbase==0.6.3.1'],
+      install_requires=['future', 'txtorcon', 'pyopenssl', 'libnacl', 'joinmarketbase==0.7.0dev'],
       python_requires='>=3.3',
       zip_safe=False)
