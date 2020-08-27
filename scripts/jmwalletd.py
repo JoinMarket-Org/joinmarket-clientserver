eb4b064dfda62598a6430b6d28f5dcce6abd7cfb
Adam Gibson <ekaggata@gmail.com>
2020-08-27 16:19:15 +0100

    encapsulate WalletDaemon as a Service

--- scripts/jmwalletd.py
+++ scripts/jmwalletd.py
@@ -9,6 +9,7 @@ from io import BytesIO
 from twisted.python.log import startLogging
 from twisted.internet import endpoints, reactor, ssl
 from twisted.web.server import Site
+from twisted.application.service import Service
 from klein import Klein
 
 from optparse import OptionParser
@@ -19,10 +20,6 @@ from jmclient import Maker, jm_single, load_program_config, \
 from jmbase.support import EXIT_ARGERROR, EXIT_FAILURE
 
 jlog = get_log()
-
-# only serving single concurrent user for now; cookie global:
-cookie = None
-wallet_service = None
 
 # for debugging; twisted.web.server.Request objects do not easily serialize:
 def print_req(request):
@@ -59,56 +56,60 @@ def response(request, succeed=True, status=200, **kwargs):
     return json.dumps(
         [{'succeed': succeed, 'status': status, **kwargs}])
 
-def check_cookie(request):
-    request_cookie = request.getHeader(b"JMCookie")
-    if cookie != request_cookie:
-        print("Invalid cookie: ", request_cookie)
-        raise NotAuthorized()
 
-def start_REST_server(port):
+
+class JMWalletDaemon(Service):
     app = Klein()
-    reactor.listenSSL(port, Site(app.resource()), contextFactory=get_ssl_context("."))
-    return app
+    def __init__(self, port, wallet_service=None):
+        # only serving single concurrent user for now;
+        # cookie tracks that single user's state.
+        self.cookie = None
+        self.port = port
+        self.wallet_service = wallet_service
 
-def jmwalletd_main():
-    import sys
-    parser = OptionParser(usage='usage: %prog [options] [wallet file]')
-    parser.add_option('-p', '--port', action='store', type='int',
-                      dest='port', default=28183,
-                      help='the port over which to serve RPC')
-    # TODO: remove the non-relevant base options:
-    add_base_options(parser)
+    def startService(self):
+        """ Encapsulates start up actions.
+        Here starting the TLS server.
+        """
+        super().startService()
+        reactor.listenSSL(self.port, Site(self.app.resource()),
+                          contextFactory=get_ssl_context("."))
 
-    (options, args) = parser.parse_args()
-
-    load_program_config(config_path=options.datadir)
-
-    app = start_REST_server(options.port)
+    def stopService(self):
+        """ Encapsulates shut down actions.
+        """
+        super().stopService()
 
     @app.handle_errors(NotAuthorized)
-    def not_authorized(request, failure):
+    def not_authorized(self, request, failure):
         request.setResponseCode(401)
         return "Invalid credentials."
 
     @app.handle_errors(NoWalletFound)
-    def no_wallet_found(request, failure):
+    def no_wallet_found(self, request, failure):
         request.setResponseCode(404)
         return "No wallet loaded."
 
+    def check_cookie(self, request):
+        request_cookie = request.getHeader(b"JMCookie")
+        if self.cookie != request_cookie:
+            print("Invalid cookie: ", request_cookie)
+            raise NotAuthorized()
+
     @app.route('/wallet/<string:walletname>/display', methods=['GET'])
-    def displaywallet(request, walletname):
+    def displaywallet(self, request, walletname):
         print_req(request)
-        check_cookie(request)
-        if not wallet_service:
+        self.check_cookie(request)
+        if not self.wallet_service:
             print("called display but no wallet loaded")
             raise NoWalletFound()
         else:
-            walletinfo = wallet_display(wallet_service, False, jsonified=True)
+            walletinfo = wallet_display(self.wallet_service, False, jsonified=True)
             return response(request, walletname=walletname, walletinfo=walletinfo)
 
     # handling CORS preflight for any route:
     @app.route('/', branch=True, methods=['OPTIONS'])
-    def preflight(request):
+    def preflight(self, request):
         print_req(request)
         request.setHeader("Access-Control-Allow-Origin", "*")
         request.setHeader("Access-Control-Allow-Methods", "POST")
@@ -117,29 +118,25 @@ def jmwalletd_main():
         request.setHeader("Access-Control-Allow-Headers", "Content-Type, JMCookie")
 
     @app.route('/wallet/<string:walletname>/lock', methods=['GET'])
-    def lockwallet(request, walletname):
-        global wallet_service
+    def lockwallet(self, request, walletname):
         print_req(request)
-        check_cookie(request)
-        if not wallet_service:
+        self.check_cookie(request)
+        if not self.wallet_service:
             print("called lock but no wallet loaded")
             raise NoWalletFound()
         else:
-            wallet_service.stopService()
-            # reset local reference to null
-            wallet_service = None
+            self.wallet_service.stopService()
+            self.wallet_service = None
             # success status implicit:
             return response(request, walletname=walletname)
 
     @app.route('/wallet/<string:walletname>/unlock', methods=['POST'])
-    def unlockwallet(request, walletname):
-        global wallet_service
-        global cookie
+    def unlockwallet(self, request, walletname):
         print_req(request)
         assert isinstance(request.content, BytesIO)
         auth_json = json.loads(request.content.read().decode("utf-8"))
         password = auth_json["password"]
-        if wallet_service is None:
+        if self.wallet_service is None:
             wallet_path = get_wallet_path(walletname, None)
             try:
                 wallet = open_test_wallet_maybe(
@@ -157,18 +154,18 @@ def jmwalletd_main():
             # yet support multiple). This is maintained for as long as the
             # daemon is active (i.e. no expiry currently implemented),
             # or until the user switches to a new wallet.
-            cookie = request.getHeader(b"JMCookie")
-            if cookie is None:
+            self.cookie = request.getHeader(b"JMCookie")
+            if self.cookie is None:
                 # TODO different error class? this could mislead:
                 raise NotAuthorized()
 
             # the daemon blocks here until the wallet synchronization
             # from the blockchain interface completes; currently this is
             # fine as long as the client handles the response asynchronously:
-            wallet_service = WalletService(wallet)
-            while not wallet_service.synced:
-                wallet_service.sync_wallet(fast=True)
-            wallet_service.startService()
+            self.wallet_service = WalletService(wallet)
+            while not self.wallet_service.synced:
+                self.wallet_service.sync_wallet(fast=True)
+            self.wallet_service.startService()
             # now that the WalletService instance is active and ready to
             # respond to requests, we return the status to the client:
             return response(request, walletname=walletname, already_loaded=False)
@@ -176,11 +173,27 @@ def jmwalletd_main():
             print('wallet was already unlocked.')
             return response(request, walletname=walletname, already_loaded=True)
 
+def jmwalletd_main():
+    import sys
+    parser = OptionParser(usage='usage: %prog [options] [wallet file]')
+    parser.add_option('-p', '--port', action='store', type='int',
+                      dest='port', default=28183,
+                      help='the port over which to serve RPC')
+    # TODO: remove the non-relevant base options:
+    add_base_options(parser)
+
+    (options, args) = parser.parse_args()
+
+    load_program_config(config_path=options.datadir)
+
     if jm_single().bc_interface is None:
         jlog.error("Running jmwallet-daemon requires configured " +
                    "blockchain source.")
         sys.exit(EXIT_FAILURE)
     jlog.info("Starting jmwalletd")
+
+    jm_wallet_daemon = JMWalletDaemon(options.port)
+    jm_wallet_daemon.startService()
 
     nodaemon = jm_single().config.getint("DAEMON", "no_daemon")
     daemon = True if nodaemon == 1 else False
