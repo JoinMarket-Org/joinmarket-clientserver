b1ac4f625cfb29ed2d05d8c06b5e3467fab7af63
Kristaps Kaupe <kristaps@blogiem.lv>
2021-02-10 16:53:20 +0200

    Pin cryptography module to v3.3.2
    Newer versions instroduces Rust as a dependency, which causes problems
    for some people / platforms -
    https://github.com/pyca/cryptography/issues/5771 .

--- jmdaemon/setup.py
+++ jmdaemon/setup.py
@@ -9,6 +9,6 @@ setup(name='joinmarketdaemon',
       author_email='',
       license='GPL',
       packages=['jmdaemon'],
-      install_requires=['txtorcon', 'pyopenssl', 'libnacl', 'joinmarketbase==0.8.2dev'],
+      install_requires=['txtorcon', 'cryptography==3.3.2', 'pyopenssl', 'libnacl', 'joinmarketbase==0.8.2dev'],
       python_requires='>=3.6',
       zip_safe=False)
