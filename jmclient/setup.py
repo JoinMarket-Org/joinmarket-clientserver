cde95f882912558bcfc38ce076405fa47da867d3
Adam Gibson <ekaggata@gmail.com>
2021-06-28 14:49:01 +0100

    update version to 0.9.0dev

--- jmclient/setup.py
+++ jmclient/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 setup(name='joinmarketclient',
-      version='0.8.3',
+      version='0.9.0dev',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='http://github.com/Joinmarket-Org/joinmarket-clientserver/jmclient',
       author='',
       author_email='',
       license='GPL',
       packages=['jmclient'],
-      install_requires=['joinmarketbase==0.8.3', 'mnemonic', 'argon2_cffi',
+      install_requires=['joinmarketbase==0.9.0dev', 'mnemonic', 'argon2_cffi',
                         'bencoder.pyx>=2.0.0', 'pyaes'],
       python_requires='>=3.6',
       zip_safe=False)
