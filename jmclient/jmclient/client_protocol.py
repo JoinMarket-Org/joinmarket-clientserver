JoinMarket-Org/joinmarket-clientserver

commit 7e73e4caa9d03b9237ad322cd5ba02e877778458

Add websocket for subscription, OpenAPI spec
1. Moves the JMWalletDaemon service class into
the jmclient package (see the wallet_rpc.py module).
2. Adds dependencies "klein" and "autobahn" to the
jmclient package, as well as "pyjwt".
3. Adds another module websocketserver.py, using
autobahn, to allow the JMWalletDaemon service to
serve subscriptions over a websocket, for e.g.
transaction notifications.
4. Adds tests both for the websocket connection
and for the JSON-RPC HTTP connection.

JmwalletdWebSocketServerFactory.sendTxNotification
sends the json-ified transaction details using
jmbitcoin.human_readable_transaction (as is currently
used in our CLI), along with the txid.
Also adds a coinjoin state update event sent via
the websocket (switch from taker/maker/none).
Require authentication to connect to websocket.

5. Add OpenApi definition of API in yaml;
also auto-create human-readable API docs in markdown.
6. Add fidelity bond function to API
7. Add config read/write route to API
8. Remove snicker rpc calls temporarily
9. Updates to docoinjoin: corrects taker_finished
   for this custom case, does not shut down at end.
10. Address detailed review comments of @PulpCattel.

jmclient/jmclient/client_protocol.py
@@ -799,7 +799,16 @@ def start_reactor(host, port, factory=None, snickerfactory=None,
     #startLogging(stdout)
     global daemon_serving_host
     global daemon_serving_port
-    usessl = True if jm_single().config.get("DAEMON", "use_ssl") != 'false' else False
+
+    # in case we are starting connections but not the
+    # reactor, we can return a handle to the connections so
+    # that they can be cleaned up properly.
+    # TODO: currently *only* used in tests, with only one
+    # server protocol listening.
+    serverconn = None
+    clientconn = None
+
+    usessl = jm_single().config.get("DAEMON", "use_ssl") != 'false'
     jmcport, snickerport, bip78port = [port]*3
     if daemon:
         try:
@@ -824,7 +833,7 @@ def start_reactor(host, port, factory=None, snickerfactory=None,
             orgp = p[0]
             while True:
                 try:
-                    start_daemon(host, p[0] - port_offset, f, usessl,
+                    serverconn = start_daemon(host, p[0] - port_offset, f, usessl,
                         './ssl/key.pem', './ssl/cert.pem')
                     jlog.info("{} daemon listening on port {}".format(
                         name, str(p[0] - port_offset)))
@@ -837,18 +846,19 @@ def start_reactor(host, port, factory=None, snickerfactory=None,
                                    "listen on any of them. Quitting.")
                         sys.exit(EXIT_FAILURE)
                     p[0] += 1
-            return p[0]
-
+            return (p[0], serverconn)
 
         if jm_coinjoin:
             # TODO either re-apply this port incrementing logic
             # to other protocols, or re-work how the ports work entirely.
-            jmcport = start_daemon_on_port(port_a, dfactory, "Joinmarket", 0)
+            jmcport, serverconn = start_daemon_on_port(port_a, dfactory,
+                                                       "Joinmarket", 0)
             daemon_serving_port = jmcport
             daemon_serving_host = host
         # (See above) For now these other two are just on ports that are 1K offsets.
         if snickerfactory:
-            snickerport = start_daemon_on_port(port_a, sdfactory, "SNICKER", 1000) - 1000
+            snickerport, serverconn = start_daemon_on_port(port_a, sdfactory,
+                                                    "SNICKER", 1000) - 1000
         if bip78:
             start_daemon_on_port(port_a, bip78factory, "BIP78", 2000)
 
@@ -863,17 +873,18 @@ def start_reactor(host, port, factory=None, snickerfactory=None,
     # starts in jmclient.payjoin:
     if usessl:
         if factory:
-                reactor.connectSSL(host, jmcport, factory, ClientContextFactory())
+            reactor.connectSSL(host, jmcport, factory, ClientContextFactory())
         if snickerfactory:
-                reactor.connectSSL(host, snickerport, snickerfactory,
+            reactor.connectSSL(host, snickerport, snickerfactory,
                                ClientContextFactory())
     else:
         if factory:
-                reactor.connectTCP(host, jmcport, factory)
+            clientconn = reactor.connectTCP(host, jmcport, factory)
         if snickerfactory:
-                reactor.connectTCP(host, snickerport, snickerfactory)
+            reactor.connectTCP(host, snickerport, snickerfactory)
     if rs:
         if not gui:
             reactor.run(installSignalHandlers=ish)
         if isinstance(jm_single().bc_interface, RegtestBitcoinCoreInterface):
             jm_single().bc_interface.shutdown_signal = True
+    return (serverconn, clientconn)

jmclient/jmclient/__init__.py
@@ -60,10 +60,13 @@ from .wallet_utils import (
 from .wallet_service import WalletService
 from .maker import Maker
 from .yieldgenerator import YieldGenerator, YieldGeneratorBasic, ygmain, \
-     YieldGeneratorService
+     YieldGeneratorService, YieldGeneratorServiceSetupFailed
 from .snicker_receiver import SNICKERError, SNICKERReceiver, SNICKERReceiverService
-from .payjoin import (parse_payjoin_setup, send_payjoin, PayjoinServer,
+from .payjoin import (parse_payjoin_setup, send_payjoin,
                       JMBIP78ReceiverManager)
+from .websocketserver import JmwalletdWebSocketServerFactory, \
+     JmwalletdWebSocketServerProtocol
+from .wallet_rpc import JMWalletDaemon
 # Set default logging handler to avoid "No handler found" warnings.
 
 try:

jmclient/jmclient/maker.py
@@ -4,7 +4,7 @@ import abc
 import atexit
 
 import jmbitcoin as btc
-from jmbase import bintohex, hexbin, get_log, EXIT_FAILURE, stop_reactor
+from jmbase import bintohex, hexbin, get_log, EXIT_FAILURE
 from jmclient.wallet_service import WalletService
 from jmclient.configure import jm_single
 from jmclient.support import calc_cj_fee
@@ -39,12 +39,18 @@ class Maker(object):
         if not self.wallet_service.synced:
             return
         self.freeze_timelocked_utxos()
-        self.offerlist = self.create_my_orders()
+        try:
+            self.offerlist = self.create_my_orders()
+        except AssertionError:
+            jlog.error("Failed to create offers.")
+            self.aborted = True
+            return
         self.fidelity_bond = self.get_fidelity_bond_template()
         self.sync_wait_loop.stop()
         if not self.offerlist:
-            jlog.info("Failed to create offers, giving up.")
-            stop_reactor()
+            jlog.error("Failed to create offers.")
+            self.aborted = True
+            return
         jlog.info('offerlist={}'.format(self.offerlist))
 
     @hexbin

jmclient/jmclient/snicker_receiver.py
@@ -7,7 +7,6 @@ import jmbitcoin as btc
 from jmclient.configure import jm_single
 from jmbase import (get_log, utxo_to_utxostr,
                     hextobin, bintohex)
-from twisted.application.service import Service
 
 jlog = get_log()
 
@@ -48,9 +47,7 @@ class SNICKERReceiverService(Service):
         super().stopService()
 
     def isRunning(self):
-        if self.running == 1:
-            return True
-        return False
+        return self.running == 1
 
 class SNICKERReceiver(object):
     supported_flags = []

jmclient/jmclient/taker_utils.py
@@ -21,7 +21,7 @@ Utility functions for tumbler-style takers;
 Currently re-used by CLI script tumbler.py and joinmarket-qt
 """
 
-def direct_send(wallet_service, amount, mixdepth, destination, answeryes=True,
+def direct_send(wallet_service, amount, mixdepth, destination, answeryes=False,
                 accept_callback=None, info_callback=None, error_callback=None,
                 return_transaction=False, with_final_psbt=False,
                 optin_rbf=False, custom_change_addr=None):
@@ -189,19 +189,15 @@ def direct_send(wallet_service, amount, mixdepth, destination, answeryes=True,
         log.info(sending_info)
         if not answeryes:
             if not accept_callback:
-                
                 if input('Would you like to push to the network? (y/n):')[0] != 'y':
-                    
                     log.info("You chose not to broadcast the transaction, quitting.")
                     return False
             else:
-                
                 accepted = accept_callback(human_readable_transaction(tx),
                                            destination, actual_amount, fee_est,
                                            custom_change_addr)
                 if not accepted:
                     return False
-        print("here is ",jm_single().bc_interface.pushtx(tx.serialize()))
         if jm_single().bc_interface.pushtx(tx.serialize()):
             txid = bintohex(tx.GetTxid()[::-1])
             successmsg = "Transaction sent: " + txid

jmclient/jmclient/wallet_service.py
@@ -714,7 +714,7 @@ class WalletService(Service):
         import_needed = self.bci.import_addresses_if_needed(addresses,
             wallet_name)
         if import_needed:
-            #self.display_rescan_message_and_system_exit(self.restart_callback)
+            self.display_rescan_message_and_system_exit(self.restart_callback)
             return
 
         if isinstance(self.wallet, FidelityBondMixin):

commit 840189cd0a0e6521f7d4009488c11dcf5180931d

Fix SNICKER daemon startup
Bug was introduced with 7e73e4caa9d03b9237ad322cd5ba02e877778458 when
return value of `start_daemon_on_port()` was changed from `int` to
`tuple`.

jmclient/jmclient/client_protocol.py
@@ -860,7 +860,8 @@ def start_reactor(host, port, factory=None, snickerfactory=None,
         # (See above) For now these other two are just on ports that are 1K offsets.
         if snickerfactory:
             snickerport, serverconn = start_daemon_on_port(port_a, sdfactory,
-                                                    "SNICKER", 1000) - 1000
+                                                    "SNICKER", 1000)
+            snickerport = snickerport - 1000
         if bip78:
             start_daemon_on_port(port_a, bip78factory, "BIP78", 2000)
 