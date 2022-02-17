321a79271219f16b0080743105a6a0ac5601a96a
Adam Gibson <ekaggata@gmail.com>
2022-02-17 16:05:33 +0000

    Add release notes and version for 0.9.5

--- jmqtui/setup.py
+++ jmqtui/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='joinmarketui',
-      version='0.9.5dev',
+      version='0.9.5',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='https://github.com/JoinMarket-Org/joinmarket-clientserver/tree/master/jmqtui',
       author='',
