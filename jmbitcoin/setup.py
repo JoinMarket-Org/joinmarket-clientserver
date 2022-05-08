7254149353e618dc7c83f44cfc47bd4063ca2a37
Adam Gibson <ekaggata@gmail.com>
2022-05-08 23:02:07 +0100

    update version for 0.9.7dev

--- jmbitcoin/setup.py
+++ jmbitcoin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='joinmarketbitcoin',
-      version='0.9.6',
+      version='0.9.7dev',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='https://github.com/JoinMarket-Org/joinmarket-clientserver/tree/master/jmbitcoin',
       author='',
