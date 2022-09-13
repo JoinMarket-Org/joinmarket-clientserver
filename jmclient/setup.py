a7e3eed318e42955f0a02abc580b50f787bc01c0
Adam Gibson <ekaggata@gmail.com>
2022-09-13 16:48:40 +0100

    add release notes and version for 0.9.8

--- jmclient/setup.py
+++ jmclient/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 setup(name='joinmarketclient',
-      version='0.9.8dev',
+      version='0.9.8',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='https://github.com/JoinMarket-Org/joinmarket-clientserver/tree/master/jmclient',
       author='',
       author_email='',
       license='GPL',
       packages=['jmclient'],
-      install_requires=['joinmarketbase==0.9.8dev', 'mnemonic', 'argon2_cffi',
+      install_requires=['joinmarketbase==0.9.8', 'mnemonic', 'argon2_cffi',
                         'bencoder.pyx>=2.0.0', 'pyaes', 'klein==20.6.0',
                         'pyjwt==2.4.0', 'autobahn==20.12.3'],
       python_requires='>=3.6',
