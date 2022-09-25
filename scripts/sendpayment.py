JoinMarket-Org/joinmarket-clientserver

commit 83c6bc053a8f5a10c00bedb4c2c9d7acb9afdb8e

Allow bitcoin: prefix for addresses with sendpayment.py

scripts/sendpayment.py
@@ -96,6 +96,12 @@ def main():
                 sweeping = True
             destaddr = args[2]
         mixdepth = options.mixdepth
+        if btc.is_bip21_uri(args[2]):
+            parsed = btc.decode_bip21_uri(args[2])
+            if 'amount' in parsed:
+                parser.error("Specify amount as a separate argument or amount in BIP21 URI, not both.")
+                sys.exit(EXIT_ARGERROR)
+            destaddr = parsed['address']
         addr_valid, errormsg = validate_address(destaddr)
         command_to_burn = (is_burn_destination(destaddr) and sweeping and
             options.makercount == 0)

commit 9d9035b43fbac4aa9969cbd2a64a234d3736f923

Fix sendpayment without amount argument, BIP21 only
Fixes bug introduced with 83c6bc053a8f5a10c00bedb4c2c9d7acb9afdb8e

scripts/sendpayment.py
@@ -96,7 +96,7 @@ def main():
                 sweeping = True
             destaddr = args[2]
         mixdepth = options.mixdepth
-        if btc.is_bip21_uri(args[2]):
+        if len(args) > 2 and btc.is_bip21_uri(args[2]):
             parsed = btc.decode_bip21_uri(args[2])
             if 'amount' in parsed:
                 parser.error("Specify amount as a separate argument or amount in BIP21 URI, not both.")