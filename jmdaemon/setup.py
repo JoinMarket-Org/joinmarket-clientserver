9f10af3adfb5af471a5d21c5dd4a48645106b28c
Kristaps Kaupe <kristaps@blogiem.lv>
2023-08-28 21:24:18 +0300

    Release notes and version for v0.9.10

--- jmdaemon/setup.py
+++ jmdaemon/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='joinmarketdaemon',
-      version='0.9.10dev',
+      version='0.9.10',
       description='Joinmarket client library for Bitcoin coinjoins',
       url='https://github.com/JoinMarket-Org/joinmarket-clientserver/tree/master/jmdaemon',
       author='',
@@ -13,6 +13,6 @@ setup(name='joinmarketdaemon',
                         'cryptography==3.3.2; platform_machine != "aarch64" and platform_machine != "amd64" and platform_machine != "x86_64"',
                         'cryptography==41.0.2; platform_machine == "aarch64" or platform_machine == "amd64" or platform_machine == "x86_64"',
                         'pyopenssl==23.2.0', 'libnacl==1.8.0',
-                        'joinmarketbase==0.9.10dev'],
+                        'joinmarketbase==0.9.10'],
       python_requires='>=3.7',
       zip_safe=False)
